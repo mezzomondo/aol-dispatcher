da42cca9a47d9a9734e6c2364af859b0e603dd52
Miki Fossati <m3zz0m0nd0@gmail.com>
2019-02-01 18:32:53 +0000

    Get rid of imperative programming in life

--- client.php
+++ client.php
@@ -9,13 +9,14 @@ $client->connect("tcp://127.0.0.1:53939");
 $poll = new ZMQPoll();
 $poll->add($client, ZMQ::POLL_IN);
 
-$read = $write = [];
-
-for ($i = 0; $i < 4; $i++) {
-    $client->send(str_shuffle("BUFFERONE"));
-    $events = $poll->poll($read, $write, 3 * 1000);
+$messages = array_map(function ($x) {return str_shuffle($x);}, array_fill(0, 3, "MEGABUFFER"));
+array_map(function($msg) use ($client, $poll) {
+    $read = $write = [];
+    $client->send($msg);
+    $events = $poll->poll($read, $write, 3 * 1000);    
     if ($events) {
         $reply = $client->recv();
         echo "Reply: {$reply}".PHP_EOL;
     }
-}
+    return;
+}, $messages);
